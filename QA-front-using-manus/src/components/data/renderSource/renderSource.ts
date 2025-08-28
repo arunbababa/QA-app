ae705737f8ff8f3c48bffc8985ffe34f8675d584
arunbababa <hatuki.1.gzs@icloud.com>
2025-08-28 12:30:51 +0900

    ファイル名変更:renderSoruceファイルの各ファイル形式の命名は単数形に統一

--- QA-front-using-manus/src/components/data/renderSource/renderSource.ts
+++ QA-front-using-manus/src/components/data/renderSource/renderSource.ts
@@ -1,9 +1,12 @@
-import png from "./IMAGEs/PNGs";
+import png from "./IMAGEs/PNG";
 import mp4 from "./VIDEOs/MP4";
 
 const fileSources = {
+  // 動画
+  mp4,
+  // 画像
   png,
-  mp4
+
 }
 
 export default fileSources
